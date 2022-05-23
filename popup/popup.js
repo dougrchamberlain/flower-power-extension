64522ea161779dc9f435e8358b0288dfbf78a72e
Doug Chamberlain <dougrchamberlain@gmail.com>
2022-05-23 12:32:37 -0400

    added firebase.

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,9 @@
+import { doc, getDoc } from "firebase/firestore";
+
 const sendMessageId = document.getElementById("updateAmount");
 if (sendMessageId) {
     sendMessageId.onclick = function() {
+        
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(
                 tabs[0].id,
